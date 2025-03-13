387f5a12785dec164f61375e1bde95a0c80793aa
John Paul Villanueva <Jampoy79@gmail.com>
2025-03-13 12:55:41 +0800

    Added Nav-bar

--- Kyobi_Travel_Agency/src/app/components/nav-bar/nav-bar.component.ts
+++ Kyobi_Travel_Agency/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [MatIconModule,
+    MatButtonModule,
+    MatToolbarModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
