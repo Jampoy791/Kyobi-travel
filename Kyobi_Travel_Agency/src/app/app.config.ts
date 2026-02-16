2aa737933a15f6dcced524caca5e386d9aa71975
John Paul Villanueva <32948820+Jampoy791@users.noreply.github.com>
2026-02-16 10:07:11 +0800

    Updated a few codes

--- Kyobi_Travel_Agency/src/app/app.config.ts
+++ Kyobi_Travel_Agency/src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), 
+    HomeBannerComponent, IntroductionComponent, provideHttpClient()]
 };
--- Kyobi_Travel_Agency/src/app/app.routes.ts
+++ Kyobi_Travel_Agency/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 
 
 export const routes: Routes = [
 
-   
-
+   {path: '', component: HomeBannerComponent},
+   {path: 'introduction', component: IntroductionComponent}
 ];
