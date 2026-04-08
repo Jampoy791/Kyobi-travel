31232910a0f1b1efff70a86809825b8cb3afbcc4
John Paul Villanueva <32948820+Jampoy791@users.noreply.github.com>
2026-04-08 15:11:39 +0800

    Update 1.5
    Updated New Ui, including new Pages, components and assets
    
    issues: mobile view not working still need to fix over the next updates
    
    Things to do: fix every pages and migrate all the scripts to its designated Components structure.

--- Kyobi_Travel_Agency/src/app/app.routes.ts
+++ Kyobi_Travel_Agency/src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
-import { HomeBannerComponent } from './components/home-banner/home-banner.component';
-import { IntroductionComponent } from './components/introduction/introduction.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PackagesComponent } from './pages/packages/packages.component';
+import { BudgetComponent } from './pages/budget/budget.component';
+import { BookingComponent } from './pages/booking/booking.component';
+import { AboutComponent } from './pages/about/about.component';
+
 
 
 
 export const routes: Routes = [
 
-   {path: '', component: HomeBannerComponent},
-   {path: 'introduction', component: IntroductionComponent}
+   { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'packages', component: PackagesComponent },
+  { path: 'budget', component: BudgetComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'about', component: AboutComponent },
 ];
